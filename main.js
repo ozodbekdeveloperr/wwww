2e8d73958a49c900f67241f58e6386f9654a6e21
ozodbek <hoshimovozodbek83@gmail.com>
2024-12-14 13:22:32 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+//1-misol
+// let drinks=[
+//     { name: "lemonade", price: 50 },
+//     { name: "lime", price: 10}
+// ];
+// function sortedDrinkByPrice(drincs){
+//     return drincs.sort((a,b)=>a.price-b.price)
+// }
+// let result=sortedDrinkByPrice(drinks)
+// console.log(result);
+
+
+// 2-misol
+// function objToArray(obj){
+//     return Object.entries(obj)
+// }
+// const exampleObject = {
+//     D: 1,
+//     B: 2,
+//     C: 3
+//   };
+//   console.log(objToArray(exampleObject));
+  
+
+// 3-misol
+// let users = [
+//   { name: "John", email: "john@example.com" },
+//   { name: "Jason", email: "jason@example.com" },
+//   { name: "Jeremy", email: "jeremy@example.com" },
+//   { name: "Jacob", email: "jacob@example.com" }
+// ]
+// function getNames(users){
+//   let sum=[]
+//   for(let user of users){
+//     sum.push(user.name)
+//   }
+//   return sum
+// }
+// console.log(getNames(users));
+
+
+
+// 4-misol
+// function afterNYears(ages, n) {
+//     const sum = {};
+//     for (let person in ages) {
+//       sum[person] = ages[person] + n;
+//     }
+//     return sum
+//   }
+//   console.log(afterNYears({
+//     "Joel": 32,
+//     "Fred": 44,
+//     "Reginald": 65,
+//     "Susan": 33,
+//     "Julian": 13
+//   }, 1));
+  
